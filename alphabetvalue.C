de8941fb4250c93d5ca5f95e0cde7bbd3c9462f5
abinandhana-va <abinandhana.va@gmail.com>
2025-09-02 17:27:59 +0000

    Alphabets used for num value 's sum program added

---
+++ alphabetvalue.C
@@ -0,0 +1,36 @@
+//1.Takes a number between 1 and 10.
+//2.Finds its word form (arr).
+//3.Calculates the sum of positions of its letters in the English alphabet.
+//4.Prints that sum
+#include<stdio.h>
+#include<string.h>
+char arr[15][10]={"one",
+                  "two",
+                  "three",
+                  "four",
+                  "five",
+                  "six",
+                  "seven",
+                  "eight",
+                  "nine",
+                  "ten"};
+int countsum(int n);
+
+int main(){
+    int n,ct;
+    printf("Enter the num:");
+    scanf("%d",&n);
+    ct=countsum(n);
+    printf("\nValue of alphabets used for num %d : %d",n,ct);
+    return 0;
+}
+
+int countsum(int n){
+    int sum=0,j;
+    char ch;
+    for(j=0;j<strlen(arr[n-1]);j++){
+        ch=arr[n-1][j];
+        sum+=(ch-'a'+1);
+    }
+    return sum;
+}
